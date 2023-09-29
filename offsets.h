b0b87712b6f004d3875f56a5ecfa4e78d2b677b4
0xThug1337 <113045072+0xThug1337@users.noreply.github.com>
2023-09-29 22:07:23 +0100

    Update offsets.h
    Updated offsets to last version I found.

--- offsets.h
+++ offsets.h
@@ -2,10 +2,10 @@
 namespace Offsets
 {
 	constexpr auto
-		State = 0x92df200,  //uworld_state 
-		Key = State + 0x38, //uworld_key 
-		ProcessEvent = 0x3548490,
-		StaticFindObject = 0x355ce00,
-		TriggerVEH = 0x1b81b10,
-		GetActorBounds = 0x4d20770;  //void GetActorBounds(bool bOnlyCollidingComponents, struct FVector& Origin, struct FVector& BoxExtent, bool bIncludeFromChildActors); // Function Engine.Actor.GetActorBounds // (Final|Native|Public|HasOutParms|HasDefaults|BlueprintCallable|BlueprintPure|Const)
+		State = 0xA0ED580,  //uworld_state 
+		Key = State + 0xA0ED5B8, //uworld_key 
+		ProcessEvent = 0x3D799D0,
+		StaticFindObject = 0x3D8E070,
+		TriggerVEH = 0x1DF1E00,
+		GetActorBounds = 0x55F5F00;  //void GetActorBounds(bool bOnlyCollidingComponents, struct FVector& Origin, struct FVector& BoxExtent, bool bIncludeFromChildActors); // Function Engine.Actor.GetActorBounds // (Final|Native|Public|HasOutParms|HasDefaults|BlueprintCallable|BlueprintPure|Const)
 }
